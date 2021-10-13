b6930779d3af3340203da1472f7a3016c368dac8
Oscar <alum.oalcalae.2020@iesaliexar.org>
2021-10-13 18:15:34 +0200

    pruebass

--- src/main/java/U1/Pruebas/RomboRaro.java
+++ src/main/java/U1/Pruebas/RomboRaro.java
@@ -7,24 +7,26 @@ public class RomboRaro {
         String palabra = "PEPITO";
         String palabraInversa = "";
         int anchuraCadena = palabra.length();
-
-        for (int i = anchuraCadena; i >= 1; i--) {
+        for (int i = anchuraCadena-1; i >= 0; i--) {
             palabraInversa = palabraInversa.concat(String.valueOf(palabra.charAt(i)));
         }
-        System.out.println(palabraInversa);
+        String palabraConcatenada = palabra.concat(palabraInversa)
 
-        /*for (int i = 0; i < anchuraCadena*2; i++) {
+
+
+        for (int i = 0; i < anchuraCadena*2; i++) {
             for (int j = 0; j < anchuraCadena*2; j++) {
                 if (j==anchuraCadena-i){
                     System.out.print(palabra.substring(0,i));
-                }else if (j==anchuraCadena+i){
-                    System.out.print(palabra.substring(i,0));
-                }else {
+                }else if (i == anchuraCadena+j){
+                    System.out.print(palabra.substring(0,anchuraCadena*2-i));
+                }
+                else {
                     System.out.print(" ");
                 }
 
             }
             System.out.println();
-        }*/
+        }
     }
 }
