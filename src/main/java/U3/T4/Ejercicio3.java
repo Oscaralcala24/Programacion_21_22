991edf1bba4d29ebb35354ad641818d05cc71341
Oscar <alum.oalcalae.2020@iesalixar.org>
2021-11-18 14:27:34 +0100

    ejercicio3 array

--- src/main/java/U3/T4/Ejercicio3.java
+++ src/main/java/U3/T4/Ejercicio3.java
@@ -18,7 +18,8 @@ import java.util.Scanner;
 public class Ejercicio3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int contadorMesas = 0;
+        int mesas = 0;
+        int ocupacion = 1;
         int contadorOcupacion = 0;
         int numeroIntroducido = 0;
         int [][] arrayMesas = new int[2][10];
@@ -30,18 +31,68 @@ public class Ejercicio3 {
                 System.out.println("¿Cuantos son? Introduzca -1 para salir del programa: ");
                 numeroIntroducido = sc.nextInt();
             }
-            if (numeroIntroducido!=-1){
+            if (numeroIntroducido==-1){
                 break;
             }
-            arrayMesas[contadorMesas][contadorOcupacion] = arrayMesas[contadorMesas][numeroIntroducido];
-            contadorMesas++;
-            contadorOcupacion++;
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < arrayMesas.length; j++) {
-                    System.out.print(" | " + arrayMesas[i][j]+ " | ");
+            if (contadorOcupacion < 10){
+                arrayMesas[ocupacion][contadorOcupacion] = numeroIntroducido;
+
+                for (int i = 0; i < arrayMesas.length; i++) {
+                    for (int j = 0; j < arrayMesas[i].length; j++) {
+                        if (i ==0){
+                            arrayMesas[i][j] = j+1;
+                            System.out.print(" | " + arrayMesas[i][j]+ " | ");
+                        }else {
+                            arrayMesas[i][contadorOcupacion] = numeroIntroducido;
+                            System.out.print(" | " + arrayMesas[i][j]+ " | ");
+                        }
+                    }
+
+                    System.out.println();
                 }
-                System.out.println("\n------------------------------------");
+                contadorOcupacion++;
+                System.out.println();
+            }else {
+                boolean sumado = false;
+                boolean sinSitioGrupo = false;
+                for (int i = 0; i < arrayMesas.length; i++) {
+                    for (int j = 0; j < arrayMesas[i].length; j++) {
+                        if (i ==0 && arrayMesas[i][j] + numeroIntroducido<=4){
+                            System.out.print(" | " + arrayMesas[i][j]+ " | ");
+                        }else{
+                            if (arrayMesas[i][j] + numeroIntroducido<=4 && !sumado){
+                                arrayMesas[i][j] = arrayMesas[i][j] + numeroIntroducido;
+                                System.out.print(" | " + arrayMesas[i][j]+ " | ");
+                                sumado = true;
+                            }else if (arrayMesas[i][j] + numeroIntroducido>4 && !sumado){
+                                sinSitioGrupo= true;
+                            } else {
+                                System.out.print(" | " + arrayMesas[i][j]+ " | ");
+                            }
+                        }
+                    }
+                    if (sinSitioGrupo){
+                        System.out.println("No tenemos hueco para su grupo en este momento. Lo sentimos");
+                        break;
+                    }
+                    System.out.println();
+                }
+                boolean completo = true;
+                for (int i = 1; i < arrayMesas.length; i++) {
+
+                    for (int j = 0; j < arrayMesas[i].length; j++) {
+                        if (arrayMesas[i][j] != 4) {
+                            completo = false;
+                            break;
+                        }
+                    }
+                }
+                if (completo){
+                    System.out.println("El restaurante esta completo, lo sentimos");
+                     numeroIntroducido=-1;
+                }
             }
+
         }
     }
 }
