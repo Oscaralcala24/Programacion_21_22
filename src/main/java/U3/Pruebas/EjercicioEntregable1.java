382286960d690c5a7011f8b8239dbd5034af3b43
Oscar <alum.oalcalae.2020@iesalixar.org>
2021-11-24 14:19:12 +0100

    Prueba Entregable

--- src/main/java/U3/Pruebas/EjercicioEntregable1.java
+++ src/main/java/U3/Pruebas/EjercicioEntregable1.java
@@ -8,8 +8,45 @@ package U3.Pruebas;
 // Para facilitar el ejercicio aunque un número primo se repita lo añadiremos al vector.
 // Si no existe ningún número primo en el vector original, se devuelve un array con el número -1 como único elemento.
 
+import java.util.Arrays;
+
 public class EjercicioEntregable1 {
     public static void main(String[] args) {
+        int [] arrayEnteros = {2,3,5,6,7,7,9,10,12};
 
+        System.out.println(Arrays.toString(getPrimos(arrayEnteros)));
+        
+    }
+
+    private static int[] getPrimos(int[] arrayEnteros) {
+        int contadorPosiciones = 1;
+        int[] arrayPrimos = new int[0];
+        for (int i = 0; i < arrayEnteros.length; i++) {
+            if (comprobarPrimos(arrayEnteros[i])){
+                arrayPrimos = Arrays.copyOf(arrayPrimos, contadorPosiciones);
+                arrayPrimos[contadorPosiciones-1]=arrayEnteros[i];
+                contadorPosiciones++;
+
+            }
+        }
+        if (contadorPosiciones==0) {
+            Arrays.copyOf(arrayPrimos, 1);
+            arrayPrimos[0] = -1;
+        }
+        return arrayPrimos;
+    }
+
+    private static boolean comprobarPrimos(int numeroEntero) {
+        boolean esPrimo = true;
+        for (int i = 2; i < numeroEntero; i++){
+            if (numeroEntero%i==0){
+                esPrimo=false;
+                break;
+            }
+            if (numeroEntero==2){
+                esPrimo=true;
+            }
+        }
+        return esPrimo;
     }
 }
---
+++ src/main/java/U3/Pruebas/EjercicioEntregable2.java
@@ -0,0 +1,43 @@
+package U3.Pruebas;
+
+import java.util.Scanner;
+
+public class EjercicioEntregable2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce un numero para pasarlo a morse: ");
+        int n = sc.nextInt();
+        System.out.println("El codigo morse es : "+convierteEnMorse(n));
+    }
+
+    private static String convierteEnMorse(int n) {
+        String numero = String.valueOf(n);
+        String morse = "";
+        for (int i = 0; i < numero.length(); i++) {
+            if (numero.charAt(i) == '0'){
+                morse = morse+"_ _ _ _ _ ";
+            }else if (numero.charAt(i) == '1'){
+                morse = morse+". _ _ _ _ ";
+            }else if (numero.charAt(i) == '2'){
+                morse = morse+". . _ _ _ ";
+            }else if (numero.charAt(i) == '3'){
+                morse = morse+". . . _ _ ";
+            }else if (numero.charAt(i) == '4'){
+                morse = morse+". . . . _  ";
+            }else if (numero.charAt(i) == '5'){
+                morse = morse+". . . . . ";
+            }else if (numero.charAt(i) == '6'){
+                morse = morse+"_ . . . . ";
+            }else if (numero.charAt(i) == '7'){
+                morse = morse+"_ _ . . . ";
+            }else if (numero.charAt(i) == '8'){
+                morse = morse+"_ _ _ . . ";
+            }else if (numero.charAt(i) == '9'){
+                morse = morse+"_ _ _ _ . ";
+            }
+        }
+        return morse;
+    }
+
+
+}
