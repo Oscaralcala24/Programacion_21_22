3e381845fbc7392cf1f38c1ddb44a0560290b835
Oscar <alum.oalcalae.2020@iesaliexar.org>
2022-01-20 18:10:52 +0100

    ejercicios

--- src/main/java/U4/Tarea2/Lista.java
+++ src/main/java/U4/Tarea2/Lista.java
@@ -2,11 +2,108 @@ package U4.Tarea2;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class Lista {
-    Integer [] arrayEnteros = {1,2,3,4,5,6,7};
-    List <Integer> lista1 = new ArrayList<>(arrayEnteros);
+
+    private int capacidad;
+    private Integer[] arrayList;
+    private int elementosInsertados = 0;
+
+    public Lista() {
+        this.capacidad = 10;
+        this.arrayList = new Integer[this.capacidad];
+    }
+
+    public Lista(int capacidad) {
+        this.capacidad = capacidad;
+        this.arrayList = new Integer[this.capacidad];
+    }
+
+    void insertarNumPrincipio(Integer numeroInsertar){
+        Integer[] arrayAux = Arrays.copyOf(arrayList, arrayList.length);
+        boolean esNulo = comprobarNulo();
+        if (!esNulo) {
+            arrayList = Arrays.copyOf(arrayList, arrayList.length + 1);
+            arrayAux = Arrays.copyOf(arrayList, arrayList.length);
+        }
+        for (int i = 0; i < arrayList.length-1; i++) {
+            arrayList[i+1] = arrayAux[i];
+        }
+        arrayList[0] = numeroInsertar;
+        elementosInsertados++;
+    }
+
+    void insertarNumFinal(Integer numeroInsertar){
+        Integer[] arrayAux = Arrays.copyOf(arrayList, arrayList.length);
+        boolean esNulo = comprobarNulo();
+        if (!esNulo) {
+            arrayList = Arrays.copyOf(arrayList, arrayList.length + 1);
+            arrayAux = Arrays.copyOf(arrayList, arrayList.length);
+        }
+        arrayList[elementosInsertados] = numeroInsertar;
+        elementosInsertados++;
+    }
+
+    void insertarNumPosicion(Integer numeroInsertar, int posicion) {
+        Integer[] arrayAux = Arrays.copyOf(arrayList, arrayList.length);
+        boolean esNulo = comprobarNulo();
+        if (!esNulo) {
+            arrayList = Arrays.copyOf(arrayList, arrayList.length + 1);
+            arrayAux = Arrays.copyOf(arrayList, arrayList.length);
+        }
+        for (int i = posicion; i < arrayList.length-1; i++) {
+            arrayList[i+1] = arrayAux[i];
+        }
+        arrayList[posicion] = numeroInsertar;
+        elementosInsertados++;
+    }
+
+    void insertarListaFinal(Integer[] listaInsertar){
+        Integer[] arrayAux = Arrays.copyOf(arrayList, arrayList.length);
+        boolean esNulo = false;
+        int contadorNulo = 0;
+        int contadorlistaInsertar = 0;
+        for (int i = 0; i < arrayList.length; i++) {
+            if (arrayList[i] == null){
+                contadorNulo++;
+            }
+        }
+        if (contadorNulo >= listaInsertar.length){
+            esNulo = true;
+        }
+        if (!esNulo){
+            arrayList = Arrays.copyOf(arrayList, arrayList.length + contadorNulo);
+        }
+        for (int i = 0; i < arrayList.length; i++) {
+            if (i<=elementosInsertados){
+                    arrayList[i] = arrayAux[i];
+            }else {
+                    arrayList[i] = listaInsertar[contadorlistaInsertar];
+                    elementosInsertados++;
+            }
+        }
+    }
+
+    private boolean comprobarNulo() {
+        boolean esNulo = false;
+        for (int i = 0; i < arrayList.length; i++) {
+            if (arrayList[i] == null){
+                esNulo = true;
+            }
+        }
+        return esNulo;
+    }
+
+    public Integer[] getArrayList() {
+        return arrayList;
+    }
+
+    public int getElementosInsertados() {
+        return elementosInsertados;
+    }
+
 
 }
--- src/main/java/U4/Tarea2/Main.java
+++ src/main/java/U4/Tarea2/Main.java
@@ -1,5 +1,25 @@
 package U4.Tarea2;
 
-public class Main {
+import java.util.Arrays;
 
+public class Main {
+    public static void main(String[] args) {
+        Lista lista1 = new Lista();
+        System.out.println(lista1.getElementosInsertados());
+        lista1.insertarNumPrincipio(4);
+        lista1.insertarNumPrincipio(7);
+        lista1.insertarNumFinal(9);
+        Integer [] arrayPasar = {2,3,5,6,7};
+        lista1.insertarListaFinal(arrayPasar);
+        lista1.insertarNumFinal(34);
+        lista1.insertarNumFinal(9);
+        lista1.insertarNumPosicion(333,0);
+        lista1.insertarNumFinal(9);
+        System.out.println(lista1.getElementosInsertados());
+        lista1.insertarNumFinal(100);
+
+
+        System.out.println(lista1.getElementosInsertados());
+        System.out.println(Arrays.toString(lista1.getArrayList()));
+    }
 }
