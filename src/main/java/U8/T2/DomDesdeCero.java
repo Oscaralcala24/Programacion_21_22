ea3cf9120967029cd615df319056e3ca76f2ee46
Oscar <alum.oalcalae.2020@iesaliexar.org>
2022-04-27 18:02:09 +0200

    ejercicios

--- src/main/java/U8/T2/DomDesdeCero.java
+++ src/main/java/U8/T2/DomDesdeCero.java
@@ -7,10 +7,7 @@ import org.w3c.dom.Node;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
@@ -62,8 +59,14 @@ public class DomDesdeCero {
                 nAux.appendChild(puesto);
                 root.appendChild(nAux);
             }
+            doc.appendChild(root);
             TransformerFactory factory = TransformerFactory.newInstance();
             Transformer transformer = factory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+            transformer.setOutputProperty("http://www.oracle.com/xml/is-standalone", "yes");
             DOMSource xml = new DOMSource(doc);
             StreamResult s = new StreamResult(new File("C:\\Users\\se_os\\Escritorio\\Programacion_21_22\\src\\main\\java\\U8\\T2\\domcero.xml"));
             transformer.transform(xml,s);
