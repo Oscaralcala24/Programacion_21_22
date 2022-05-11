8eb12136ead4e82bd24e317e6e7ea0c7d8793486
Oscar <alum.oalcalae.2020@iesaliexar.org>
2022-05-11 13:19:40 +0200

    ejercicioss

--- src/main/java/U9/Docker/Tarea2/Main.java
+++ src/main/java/U9/Docker/Tarea2/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         ADClassicModels adc = new ADClassicModels();
         System.out.println(adc.getEmpleados());
+        System.out.println("------------------------------------------");
+        System.out.println("------------------------------------------");
+        System.out.println("------------------------------------------");
+        System.out.println("------------------------------------------");
         System.out.println(adc.getOffice());
     }
 }
